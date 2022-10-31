d9d837cdb0465c9bc40219748707ff806efdbde6
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-11-01 02:50:29 +0500

    update

---
+++ Listbox/Listbox/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb= new StringBuilder();
+            foreach(object item in listBox1.SelectedItems)
+            {
+                listBox2.Items.Add(item.ToString());
+                sb.Append(item.ToString()); 
+                sb.Append(" ");
+            };
+
+            MessageBox.Show("Select the items first", sb.ToString());
+
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
