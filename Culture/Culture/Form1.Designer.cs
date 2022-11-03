6b67b485366edfb459cc46a3861499150552d533
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-11-04 01:08:32 +0500

    Update

---
+++ Culture/Culture/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Culture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSaved = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataSet1 = new Culture.DataSet1();
+            this.languageBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.englishDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.germanDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.languageBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSaved
+            // 
+            this.btnSaved.Location = new System.Drawing.Point(704, 313);
+            this.btnSaved.Name = "btnSaved";
+            this.btnSaved.Size = new System.Drawing.Size(75, 23);
+            this.btnSaved.TabIndex = 0;
+            this.btnSaved.Text = "Saved";
+            this.btnSaved.UseVisualStyleBackColor = true;
+            this.btnSaved.Click += new System.EventHandler(this.btnSaved_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.englishDataGridViewTextBoxColumn,
+            this.germanDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.languageBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 267);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 346);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // languageBindingSource
+            // 
+            this.languageBindingSource.DataMember = "Language";
+            this.languageBindingSource.DataSource = this.dataSet1;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // englishDataGridViewTextBoxColumn
+            // 
+            this.englishDataGridViewTextBoxColumn.DataPropertyName = "English";
+            this.englishDataGridViewTextBoxColumn.HeaderText = "English";
+            this.englishDataGridViewTextBoxColumn.Name = "englishDataGridViewTextBoxColumn";
+            // 
+            // germanDataGridViewTextBoxColumn
+            // 
+            this.germanDataGridViewTextBoxColumn.DataPropertyName = "German";
+            this.germanDataGridViewTextBoxColumn.HeaderText = "German";
+            this.germanDataGridViewTextBoxColumn.Name = "germanDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSaved);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.languageBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaved;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn englishDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn germanDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource languageBindingSource;
+        private DataSet1 dataSet1;
+    }
+}
+
---
+++ Culture/Culture/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Culture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaved_Click(object sender, EventArgs e)
+        {
+            dataSet1.WriteXml(string.Format("{0}/data.xml", Application.StartupPath));
+            ResourceWriter ren = new ResourceWriter(Application.StartupPath + "/resource.en-US.resources");
+            ResourceWriter rde = new ResourceWriter(Application.StartupPath + "/resource.de-DE.resources");
+            foreach (DataSet1.LanguageRow row in dataSet1.Language.Rows)
+            {
+                ren.AddResource(row.ID, row.English);
+                rde.AddResource(row.ID, row.German);
+            }
+            ren.Generate();
+            ren.Close();
+            rde.Generate();
+            rde.Close();
+
+            MessageBox.Show("Successfully saved.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataSet1.ReadXml(string.Format("{0}/data.xml", Application.StartupPath));
+            CultureInfo ci = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+            ResourceManager rm = ResourceManager.CreateFileBasedResourceManager("resource", Application.StartupPath, null);
+            if (rm.GetString("001") != null)
+
+                label1.Text = rm.GetString("001");
+        }
+    }
+}
