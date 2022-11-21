29683c6a7cef199075a3a736454f621889fabbab
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-11-21 16:37:49 +0500

    update

---
+++ ComboBox/Combo1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using ComboBox;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Combo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            mycombo1.SelectedIdexChanged += new EventHandler(mycombo1_SelectedIdexChanged);
+           
+
+        }
+        void mycombo1_SelectedIdexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(mycombo1.SlectedText);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            MessageBox.Show(mycombo1.SelectedValue.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(mycombo1.SlectedText);
+        }
+    }
+}
---
+++ ComboBox/new form/UserControl1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace new_form
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            UserControl1.SelectedIdexChanged += new EventHandler(myCombobox_SelectedIdexChanged);
+        }
+
+        void myCombobox_SelectedIdexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(UserControl1.SlectedText);
+        }
+
+        private void btnDropdownSelectedvalue_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(UserControl1.Selectedvalue.ToString());
+        }
+
+        private void btnDropDownSelectedText_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(UserControl1.SlectedText);
+        }
+    }
+}
